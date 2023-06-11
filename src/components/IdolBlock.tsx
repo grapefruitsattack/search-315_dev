e3e890d3a0ba3a26333b0afa042292e32119efb2
grapefruitsattack <teachcactustoswim+1@gmail.com>
2023-06-11 22:26:30 +0900

    グラデーション背景ホバー表示

--- src/components/IdolBlock.tsx
+++ src/components/IdolBlock.tsx
@@ -18,13 +18,15 @@ export default function IdolBlock({ id }: { id: string }) {
           // className={`group idol-block border-`+id}
           className={`group ` + borderHover.idol}
         >
-        <span className={`inline-block justify-items-center items-center transition-transform group-hover:-skew-y-3 group-hover:text-gray-600 motion-reduce:transform-none`}>
+        <div className={`inline-block justify-items-center items-center transition-transform group-hover:-skew-y-3 group-hover:text-gray-600 motion-reduce:transform-none`}>
+        <span>
         <h2 className={` text-2xl lg:text-3xl text-gray-800 font-sans m-0 max-w-[30ch]`}>
               {''}{singingInfo.find(data => data.singingInfoId === id)?.singingInfoName}
           </h2>
           <p className={`max-w-[30ch] font-semibold text-xs lg:text-sm opacity-70 text-left text-`+id}>
           {singingInfo.find(data => data.singingInfoId === id)?.singingInfoRomajiName}
           </p>
-            </span>
+        </span>
+        </div>
         </a>
     )}
