beb59a97a3207dec3b2411a98d669d6fcb4d6ff9
grapefruitsattack <teachcactustoswim+1@gmail.com>
2023-07-29 20:30:09 +0900

    フッター削除

--- src/components/HeaderAndFooter.tsx
+++ src/components/HeaderAndFooter.tsx
@@ -14,16 +14,7 @@ export default function HeaderAndFooter() {
                     </Link>
                 </code>
             </p>
-            <div className="z-50 fixed bottom-0 left-0 flex h-24 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black">
-                <a
-                    className="pointer-events-none text-xs flex place-items-center gap-2 p-0 lg:pointer-events-auto lg:p-0"
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <span>このサイトは個人によって運営されている非公式ファンサイトです</span>
-                </a>
-            </div>
+
         </div>
         );
     }
