ca07a4def198f2d33cf72bdc12b6c9b453f4cc7c
grapefruitsattack <teachcactustoswim+1@gmail.com>
2023-07-31 22:10:21 +0900

    ユニット名ボタンデザイン調整、リンク修正

--- src/components/UnitBlock.tsx
+++ src/components/UnitBlock.tsx
@@ -1,6 +1,8 @@
 
 import singingMaster from '../data/singingMaster.json';
 import borderHover from '../app/border-hover.module.css';
+import Link from 'next/link';
+import { motion } from "framer-motion";
 
 interface ItemCSS extends React.CSSProperties{
   '--c':string
@@ -9,9 +11,15 @@ interface ItemCSS extends React.CSSProperties{
 export default function UnitBlock({ id }: { id: string }) {
   const color = singingMaster.find(data => data.singingInfoId === id)?.color;
   const colorStr:string = color ===undefined ?'' : color;
+
+  const qStr: string = singingMaster.filter(
+    data=>
+      id.substring(0,3) === data.singingInfoId.substring(0,3)
+      && data.personFlg === 1
+    ).map(data=>data.singingInfoId).join(' ');
+
     return (
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <Link
           // className={`group
           // rounded-tl-3xl
           // bg-blue-950/70
@@ -21,14 +29,26 @@ export default function UnitBlock({ id }: { id: string }) {
           // hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30
           // border-`+ id + ` hover:bg-`+id+`/50`}
           style={{"--c": '#'+colorStr}as ItemCSS}
-          className={`group `+borderHover.unit}
-          target="_blank"
-          rel="noopener noreferrer"
+          className={
+            `group transition duration-500 bg-blue-950/90 rounded-r-3xl 
+            text-gray-100 hover:text-blue-950
+            hover:bg-blue-100/10
+            border-l-8 border-` + id
+            }
+            href={{ pathname: '/search', query: {q: qStr }}}
         >
-        <span className={`inline-block justify-items-center items-center transition-transform group-hover:-skew-y-3 group-hover:text-gray-600 motion-reduce:transform-none text-gray-100 `}>
+        <motion.button
+          className='w-full h-full'
+          whileHover={{
+            scale: 1.03,
+            transition: { duration: 1 },
+          }}
+          >
+        <span className={`inline-block justify-items-center items-center transition-transform  motion-reduce:transform-none `}>
         <h2 className={`my-px text-2xl lg:text-4xl break-all lg:break-normal font-mono m-0 max-w-[30ch]`}>
               {''}{singingMaster.find(data => data.singingInfoId === id)?.singingInfoName}
           </h2>
             </span>
-        </a>
+        </motion.button>
+        </Link>
     )}
