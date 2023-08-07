2840747c6beb7ecfa876dcea9e21ae9dcf74de39
grapefruitsattack <teachcactustoswim+1@gmail.com>
2023-08-07 22:20:10 +0900

    favicon設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'サーチサイコー',
+  icons:[
+    { url: "/favicons/android-chrome-192x192.png", sizes: "192x192", type: "image/png" }, 
+    { url: "/favicons/android-chrome-512x512.png", sizes: "512x512", type: "image/png" }, 
+  ]
 }
 
 export default function RootLayout({
@@ -19,6 +23,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+    <Head>
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"/>
+    <link rel="manifest" href="/favicons/site.webmanifest"/>
+    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#2b47bb"/>
+    <meta name="msapplication-TileColor" content="#2b5797"/>
+    <meta name="theme-color" content="#ffffff"/>
+    </Head>
     <body className={inter.className}>
       <Providers>
       {children}
