3ee911766a957a6137eb81d73a9c5bf6799a0b84
grapefruitsattack <teachcactustoswim+1@gmail.com>
2023-08-21 21:12:31 +0900

    曲詳細画面　試聴動画を追加

--- src/features/app/song/components/SongContent.tsx
+++ src/features/app/song/components/SongContent.tsx
@@ -94,7 +94,7 @@ export default function SongContent({ result, albumResult }: { result: SongMaste
                 lg:w-1/2 
             '>
                 {/* Youtube */}
-                {result.youtubeId===''
+                {result.youtubeId==='' && result.trialYoutubeId===''
                 ?<></>
                 :
                 <div 
@@ -104,7 +104,7 @@ export default function SongContent({ result, albumResult }: { result: SongMaste
                     `}
                 >
                     <a className=""
-                    href={`https://youtu.be/${result.youtubeId}`}
+                    href={`https://youtu.be/${result.youtubeId===''?result.trialYoutubeId:result.youtubeId}`}
                     target="_blank" rel="noopener noreferrer">
                         <motion.button
                             className='rounded-lg border-2 border-red-500 w-full h-full
@@ -123,7 +123,7 @@ export default function SongContent({ result, albumResult }: { result: SongMaste
                                 flex flex-wrap justify-center items-center font-sans font-black 
                                 mobileM:my-1 my-2
                             '>
-                                {'YouTube'}
+                                {result.youtubeId===''?'試聴動画':'YouTube'}
                                 <span className="">
                                 <svg className="inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path d="M10 6V8H5V19H16V14H18V20C18 20.5523 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V7C3 6.44772 3.44772 6 4 6H10ZM21 3V11H19L18.9999 6.413L11.2071 14.2071L9.79289 12.7929L17.5849 5H13V3H21Z"></path></svg>
                                 </span>
@@ -134,7 +134,7 @@ export default function SongContent({ result, albumResult }: { result: SongMaste
                 }
                 {/* YouTube Music */}
                 {result.youtubeId===''
-                ?<></>
+                ?result.trialYoutubeId===''?<></>:<div></div>
                 :
                 <div 
                     className={`
