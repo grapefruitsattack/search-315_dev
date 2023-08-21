193b0d964e348d0f102e6f7712827ac010552722
grapefruitsattack <teachcactustoswim+1@gmail.com>
2023-08-21 21:25:42 +0900

    アルバム詳細画面　　試聴動画を追加

--- src/features/app/album/components/AlbumContent.tsx
+++ src/features/app/album/components/AlbumContent.tsx
@@ -76,7 +76,7 @@ export default function AlbumContent({ album, }: { album: Albums}) {
                 lg:w-1/2
             '>
                 {/* Youtube */}
-                {album.youtubeId===''
+                {album.youtubeId==='' && album.trialYoutubeId===''
                 ?<></>
                 :
                 <div 
@@ -85,7 +85,7 @@ export default function AlbumContent({ album, }: { album: Albums}) {
                     `}
                 >
                     <a className="w-full"
-                    href={`https://youtube.com/playlist?list=${album.youtubeId}`}
+                    href={`${album.youtubeId===''?'https://youtu.be/'+album.trialYoutubeId:'https://youtube.com/playlist?list='+album.youtubeId}`}
                     target="_blank" rel="noopener noreferrer">
                         <motion.button
                             className='rounded-lg border-2 border-red-500 w-full h-full
@@ -104,7 +104,7 @@ export default function AlbumContent({ album, }: { album: Albums}) {
                                 flex flex-wrap justify-center items-center font-sans font-black 
                                 mobileM:my-1 my-2
                             '>
-                                {'YouTube'}
+                                {album.youtubeId===''?'試聴動画':'YouTube'}
                                 <span className="">
                                 <svg className="inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path d="M10 6V8H5V19H16V14H18V20C18 20.5523 17.5523 21 17 21H4C3.44772 21 3 20.5523 3 20V7C3 6.44772 3.44772 6 4 6H10ZM21 3V11H19L18.9999 6.413L11.2071 14.2071L9.79289 12.7929L17.5849 5H13V3H21Z"></path></svg>
                                 </span>
@@ -116,7 +116,7 @@ export default function AlbumContent({ album, }: { album: Albums}) {
                 }
                 {/* YouTube Music */}
                 {album.youtubeId===''
-                ?<></>
+                ?album.trialYoutubeId===''?<></>:<div></div>
                 :
                 <div 
                     className={`
