6cedc3ba1bb5b7287dce6b9c4c50106a07b09072
grapefruitsattack <teachcactustoswim+1@gmail.com>
2023-09-19 22:14:46 +0900

    ビルドエラー解決

--- src/app/315setlist/page.tsx
+++ src/app/315setlist/page.tsx
@@ -1,2 +1,9 @@
 import { Metadata } from 'next'
 import dynamic from "next/dynamic";
+
+export default function SaikoSetList({ params }: { params: { id: string } }) {
+
+    return (
+      <>315setlist</>
+    );
+  }
