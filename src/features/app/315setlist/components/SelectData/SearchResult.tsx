07a74512608523b20f89385715865cf973f25528
grapefruitsattack <teachcactustoswim+1@gmail.com>
2023-10-01 19:24:07 +0900

    検索結果画面編集

--- src/features/app/315setlist/components/SelectData/SearchResult.tsx
+++ src/features/app/315setlist/components/SelectData/SearchResult.tsx
@@ -7,6 +7,7 @@ import songInfoDesc from '../../../../../data/songInfoDesc.json';
 import songInfoAsc from '../../../../../data/songInfoAsc.json';
 import type { SongMaster, Albums, SongInfo, Tabs } from '../../../../../data/types';
 
+
 const STORAGE_315SETLIST_SERVICE = '315SetlistData';
 
 export default function SearchResult({searchParams}:{ searchParams: SearchParams}) {
@@ -19,25 +20,35 @@ export default function SearchResult({searchParams}:{ searchParams: SearchParams
 
 
     //保存用ローカルストレージ取得
-    const [subscService, setSubscService] = use315SetlistData(new SetListData());
+    const [selectedSetListData, setSetListData] = use315SetlistData(new SetListData());
 
     return(
     <>
     {AlbumsDesc.map((data, index)=>{
-    const useResult: SongMaster[] = ResultDesc.filter(result=>result.albumId===data);
+    const useResult: SongMaster[] = ResultAsc.filter(result=>result.albumId===data);
     return(
-    <div key={index}>{useResult[0].albumId}
-    {useResult.map((data, index)=>(
-    <div key={index}>
-    <button 
-        className='text-red-400'
-        onClick={()=>{
-            subscService.setListSongs.push(data);
-            setSubscService(subscService);
-            console.log(subscService)
-        }}
-    > {data.songTitle}</button>
-    </div>))}
+    <div key={index} className='border-b-2'>
+    {useResult[0].albumTitle}
+        {useResult.map((data, index)=>{
+            const added: boolean = selectedSetListData.setListSongs.some(song=>song.songId===data.songId);
+            return(
+            <div key={index} className='flex items-center'>
+                <button 
+                    className='text-white bg-black rounded pr-1 mb-1 h-fit'
+                    onClick={()=>{
+                        setSetListData({...selectedSetListData,setListSongs:[...selectedSetListData.setListSongs,data]});
+                    }}
+                    disabled={added}
+                > 
+                <div className={`flex items-center text-sm `}>
+                <svg className={`fill-white ${added?'hidden':''}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 24" width="16" height="16"><path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"></path></svg>
+                <svg className={`fill-white ${added?'':'hidden'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 24" width="16" height="16"><path d="M10.0007 15.1709L19.1931 5.97852L20.6073 7.39273L10.0007 17.9993L3.63672 11.6354L5.05093 10.2212L10.0007 15.1709Z"></path></svg>
+                {added?'追加済み':'追加'}</div>
+                </button>
+                <span className='pl-2 lg:w-[150px]'>{data.songTitle}</span>
+            </div>
+            )
+        })}
     
     </div>
     )})}
