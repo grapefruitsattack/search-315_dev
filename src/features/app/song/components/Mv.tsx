39e39206a862b36c69852b556735dd21f26e2527
grapefruitsattack <teachcactustoswim+1@gmail.com>
2023-10-12 21:16:51 +0900

    項目名をMVから関連動画へ変更 関連動画に振り付け講座追加

--- src/features/app/song/components/Mv.tsx
+++ src/features/app/song/components/Mv.tsx
@@ -12,6 +12,7 @@ const mvTypeNames: {
     ,{mvType:'GS_1stAnniv',mvName:'サイスタ1周年PV'}
     ,{mvType:'GS_3D_ALL',mvName:'サイスタ3Dライブまとめ'}
     ,{mvType:'GS_PROM',mvName:'サイスタ紹介プレイリスト'}
+    ,{mvType:'DANCE',mvName:'振り付け講座'}
 ]
 
 export default function Mv({ mvInfos }: { mvInfos: MvInfo[] }) {
@@ -25,7 +26,7 @@ export default function Mv({ mvInfos }: { mvInfos: MvInfo[] }) {
         >
             <svg className="mr-1 fill-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
                 <path d="M2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM4 5V19H20V5H4ZM12 12.1707V6H17V8H14V15C14 16.6569 12.6569 18 11 18C9.34315 18 8 16.6569 8 15C8 13.3431 9.34315 12 11 12C11.3506 12 11.6872 12.0602 12 12.1707Z"></path></svg>
-            {'MV'}
+            {'関連動画'}
         </div>
         <div className="flex flex-col gap-4">
             {mvInfos.map((info, index) => (
